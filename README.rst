dd65127ee25faea591db953408f65620320685e0
Abraham Toriz <awonderfulcode@gmail.com>
2018-02-12 16:36:53 -0600

    borrowed code from flask.config

---
+++ README.rst
@@ -0,0 +1,20 @@
+Itacate
+=======
+
+*Itacatl: Provisión de comida que se lleva para un viaje o un paseo, o que ha sobrado de una fiesta y se da a algunos invitados.*
+
+El módulo de configuración de flask para el resto del mundo, por que me gusta cómo funciona. Escribe tus configuraciones en archivos python e impórtalas según el entorno, sobreescribiéndolas en cascada.
+
+The `config` module from `flask` for the rest of the world, just because I like how it works. Write your config files in normal python files and import them depending on environment variables. Overwrite them in cascade.
+
+Cómo usar / Usage
+-----------------
+
+.. code-block:: python
+
+   from itacate import Config
+   import os
+
+   config = Config(os.path.dirname(os.path.realpath(__file__)))
+   config.from_pyfile('settings.py')
+   config.from_envvar('MY_APP_SETTINGS', silent=False)
---
+++ setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import io
+import re
+from setuptools import setup
+
+with io.open('README.rst', 'rt', encoding='utf8') as f:
+    readme = f.read()
+
+setup(
+    name='itacate',
+    version='1.0.0',
+    url='https://github.com/categulario/itacate',
+    license='BSD',
+    author='Abraham Toriz Cruz',
+    author_email='awonderfulcode@gmail.com',
+    description='Configuration module from flask, for the rest of the world',
+    long_description=readme,
+    packages=[],
+    platforms='any',
+    install_requires=[],
+    extras_require={
+        'dev': [
+            'pytest>=3',
+            'tox',
+        ],
+    },
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
